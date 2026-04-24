6c30b1d935537347d97f2b1789d7e6cacbc1f28c
Manogna-M236 <11249a236@kanchiuniv.ac.in>
2026-04-24 18:35:16 +0530

    knapsack.c

---
+++ knapsack.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+struct item
+{
+    int profit;
+    int weight;
+    float ratio;
+};
+
+void sort(struct item items[], int n)
+{
+    struct item temp;
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if (items[j].ratio < items[j+1].ratio)
+            {
+                temp = items[j];
+                items[j] = items[j+1];
+                items[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int c, n;
+    printf("Enter capacity: ");
+    scanf("%d", &c);
+
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    struct item itm[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("Enter item %d weight and profit: ", i+1);
+        scanf("%d %d", &itm[i].weight, &itm[i].profit);
+
+        itm[i].ratio = (float)itm[i].profit / itm[i].weight;
+    }
+
+    sort(itm, n);
+
+    float totalProfit = 0.0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if (c >= itm[i].weight)
+        {
+            totalProfit += itm[i].profit;
+            c -= itm[i].weight;
+        }
+        else
+        {
+            totalProfit += itm[i].ratio * c;
+            break;
+        }
+    }
+
+    printf("Maximum profit = %.2f\n", totalProfit);
+
+    return 0;
+}
